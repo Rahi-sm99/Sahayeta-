edf194b4d6d0cbd9ba06f89bae4862510573eaa2
Spandan Mondal <spandanmondal15@gmail.com>
2026-04-28 12:21:43 +0530

    migrated supabase config to environment variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ynptsqackqcxwygpehfj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlucHRzcWFja3FjeHd5Z3BlaGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNTI3NTEsImV4cCI6MjA5MjgyODc1MX0.SsrhlEkq6O5xilxf11iZsYUsdjyi-RkYTjORFDJD8qw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
